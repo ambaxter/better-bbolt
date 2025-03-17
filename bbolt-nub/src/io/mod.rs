ddbfeb0ca6f6c5d8439bc67036603fda87ba9d3f
Adam Baxter <adam.m.baxter@gmail.com>
2025-03-17 22:51:22 +0000

    Reworked read traits

--- bbolt-nub/src/io/mod.rs
+++ bbolt-nub/src/io/mod.rs
@@ -1,5 +1,6 @@
 use crate::common::errors::DiskReadError;
 use crate::common::id::{DiskPageId, FreelistPageId, MetaPageId, NodePageId};
+use crate::pages::bytes::LazyPage;
 use crate::pages::freelist::FreelistPage;
 use crate::pages::meta::MetaPage;
 use crate::pages::{Page, PageBytes};
@@ -9,19 +10,31 @@ pub mod disk_cache;
 
 //AsRef<[u8]>
 
-pub trait ReadData {
+pub trait ReadData: Sized {
   type Output: PageBytes;
+
   fn read_data(&self, disk_page_id: DiskPageId) -> Result<Self::Output, DiskReadError>;
 }
 
 pub trait ReadPage: ReadData {
   type PageOutput: PageBytes;
+
   fn read_meta(
     &self, meta_page_id: MetaPageId,
   ) -> Result<MetaPage<Self::PageOutput>, DiskReadError>;
+
   fn read_freelist(
     &self, freelist_page_id: FreelistPageId,
   ) -> Result<FreelistPage<Self::PageOutput>, DiskReadError>;
 
   fn read_node(&self, node_page_id: NodePageId) -> Result<Page<Self::PageOutput>, DiskReadError>;
+}
+
+pub trait ReadOverflow: ReadPage<PageOutput = LazyPage<<Self as ReadData>::Output, Self>> {
+  fn read_freelist_overflow(
+    &self, freelist_page_id: FreelistPageId, overflow: u32,
+  ) -> Result<Self::Output, DiskReadError>;
+  fn read_node_overflow(
+    &self, node_page_id: NodePageId, overflow: u32,
+  ) -> Result<Self::Output, DiskReadError>;
 }
